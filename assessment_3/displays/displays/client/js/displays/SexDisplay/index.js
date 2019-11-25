4f903e59ee2287312811a14a9cea581c5e59062b
Beth Koulyras <bkou2095@uni.sydney.edu.au>
2019-11-25 13:24:35 +1100

    Added delauney

--- assessment_3/displays/displays/client/js/displays/SexDisplay/index.js
+++ assessment_3/displays/displays/client/js/displays/SexDisplay/index.js
@@ -1,7 +1,17 @@
+import p5 from 'p5';
+
+import sexDisplayCanvas from './sexDisplayCanvas';
+
 class SexDisplay {
-    constructor() {
-        console.log('sex display');
-    }
+  constructor() {
+    console.log('sex display');
+
+    const facePoints = document.getElementById('sex');
+    facePoints.classList.add('Display__Active');
+
+    this.facePointsP5 = new p5(sexDisplayCanvas, facePoints);
+
+  }
 }
 
 export default SexDisplay;
